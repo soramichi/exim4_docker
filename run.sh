305edc51fb2236a6f104005a3513ff0eb6a1aade
Soramichi Akiyama <akiyama@m.soramichi.jp>
2018-11-11 16:32:36 +0900

    do not listen conncetions from outer world

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker run -h smtp.local -v `pwd`/log:/var/log/exim4 -p 25:25 -itd --rm exim4_soramichi
+sudo docker run -h smtp.local -v `pwd`/log:/var/log/exim4 -p 127.0.0.1:25:25 -itd --rm exim4_soramichi
